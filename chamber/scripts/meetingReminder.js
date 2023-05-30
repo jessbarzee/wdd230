18f990058944a977e0a024211599068b6a1b7041
jessbarzee <bar19007@byui.edu>
2023-05-30 17:14:26 -0600

    LOTS OF CHANGES week 5

--- chamber/scripts/meetingReminder.js
+++ chamber/scripts/meetingReminder.js
@@ -1,13 +1,11 @@
-const message = document.querySelector('#cta');
-const currentDay = new Date(Date.now()).getDay();
+var message = document.querySelector('span');
+var d = new Date();     
+var dayOfWeek = d.getDay();
 
-if (currentDay == [1]) {
-    message = '🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.'
-}
-if (currentDay ==  [2]) {
-    message = '🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.'
+if (dayOfWeek == 1 || dayOfWeek == 2) {
+    message.textContent = '🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.';
+    message.style.display = "block"
 }
 else {
-    //do nothing
+    message.style.display = "none"
 }
-
