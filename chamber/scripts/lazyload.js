8acda2dc79bb4bef7270495712358e1c899f47d6
jessbarzee <bar19007@byui.edu>
2023-06-06 17:04:39 -0600

    windchill and days visited

--- chamber/scripts/lazyload.js
+++ chamber/scripts/lazyload.js
@@ -24,3 +24,23 @@ if ("IntersectionObserver" in window) {
     loadImages(img)
   })
 }
+
+
+// LOCAL STORAGE
+
+const MILLIS_PER_DAY = 24 * 60 * 60 * 1000
+
+let today = new Date()
+let lastVisit = localStorage.getItem("visit");
+let visitSpan = document.querySelector(".visit");
+
+if (lastVisit != null) {
+  let lastVisitDate = new Date(lastVisit)
+  let daysSinceLastVisit = Math.floor((today.getTime() - lastVisitDate.getTime()) / MILLIS_PER_DAY)
+  visitSpan.textContent = daysSinceLastVisit
+} else {
+  visitSpan.textContent = 0
+}
+
+localStorage.setItem("visit", today)
+
--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,26 +1,19 @@
 // get the two input values of temperature and wind speed
-
-// get value for temperature
-function temperature(min, max) {
-	return Math.floor(Math.random() * (max - min) + min);
-}
-console.log("Random Number between 1 and 100: ")
-
-console.log( windspeed(1, 100) );
-
-// get value for windspeed
-function windspeed(min, max) {
-	return Math.floor(Math.random() * (max - min) + min);
-}
-
-console.log("Random Number between 1 and 100: ")
-
-console.log( windspeed(1, 50) );
-
-
+let temperature = document.querySelector("#temperature").textContent
+let speed = document.querySelector("#speed").textContent
+let windChill = document.querySelector("#windChill")
 
 // check to make sure they meet the specification limits  (<=50°F and >3.0mph) allowed to officially calculate the wind chill
+
 
 // either calculate and display the wind chill factor value or display "N/A" (not applicable) if
 
 // the input values did not meet the requirements.
+// windspeed = 35.74 + 0.6125t - 35.75s^0.16+ 0.4275ts^0.16 
+console.log(temperature, speed)
+if (temperature <= 50 && speed >= 3){
+    let chill = Math.round((35.75 + (0.6215 *temperature)) - (35.75 * Math.pow(speed,0.16))+(0.4275*temperature*Math.pow(speed,0.16)));
+    windChill.textContent = chill
+}else {
+    windChill.textContent = "N/A"
+}
