18b59e690a9a23d3f41a69d81c41691d010302b5
jessbarzee <bar19007@byui.edu>
2023-06-22 19:01:50 -0600

    lesson 10 dinal commit

--- lesson10/scripts/weather.js
+++ lesson10/scripts/weather.js
@@ -3,7 +3,7 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?q={Fairbanks}&appid={8adace9cc107a4744eebee516233a46e}';
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=8adace9cc107a4744eebee516233a46e';
 
 async function apiFetch() {
     try {
@@ -11,13 +11,25 @@ async function apiFetch() {
       if (response.ok) {
         const data = await response.json();
         console.log(data); // this is for testing the call
-        // displayResults(data);
+        displayResults(data);
       } else {
           throw Error(await response.text());
       }
     } catch (error) {
         console.log(error);
     }
-  }
+}
   
     apiFetch();
+
+function displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+
+}
