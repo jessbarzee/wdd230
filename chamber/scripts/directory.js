c337e77e3d0a509bb64f1153b5ac8123b1ec95d7
jessbarzee <bar19007@byui.edu>
2023-06-30 14:14:42 -0600

    contact page

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -18,17 +18,14 @@ const displayData = ((directory) => {
       let h3 = document.createElement('h3');
       let p = document.createElement('p');
       let p2 = document.createElement('p');
-      let p3 = document.createElement('p');
-      let p4 = document.createElement('p');
-    //   let p5 = document.createElement('p');
+      let a = document.createElement('a');
       let logo = document.createElement('img');
   
       // Build the h2 content out to show the prophet's full name - finish the template string
       h3.textContent = `${business.businessName}`;
       p.textContent =`${business.address}`;
       p2.textContent =`Membership Level: ${business.membership_lvl}`;
-      p3.textContent =`Person of Contact: ${business.contactName}`;
-      p4.textContent=`Website: ${business.websiteurl}`;
+      a.textContent=`Website: ${business.websiteurl}`;
       
   
       // Build the image portrait by setting all the relevant attribute
@@ -39,16 +36,14 @@ const displayData = ((directory) => {
       logo.setAttribute('height', 'auto');
       
 
-    //   p2.setAttribute('a', 'href');
-    //   p2.setAttribute('src', directory.websiteurl);
+      a.setAttribute('href', directory.websiteurl);
   
       // Append the section(card) with the created elements
       card.appendChild(logo);
       card.appendChild(h3);
       card.appendChild(p2);
-      card.appendChild(p3)
       card.appendChild(p);
-      card.appendChild(p4)
+      card.appendChild(a)
       
   
       cards.appendChild(card);
@@ -56,51 +51,4 @@ const displayData = ((directory) => {
 })
 
 
-
-
-// For Home page Spotlight section
-
-// const displayData = ((directory) => {
-//   const cards = document.querySelector('div.spotlights'); // select the output container element
-
-//   directory.forEach((business) => {
-//     // Create elements to add to the div.cards element
-//     let card = document.createElement('section');
-//     let h3 = document.createElement('h3');
-//     let p = document.createElement('p');
-//     let p2 = document.createElement('p');
-//     let p3 = document.createElement('p');
-//   //   let p4 = document.createElement('p');
-//   //   let p5 = document.createElement('p');
-//     let logo = document.createElement('img');
-
-//     // Build the h2 content out to show the prophet's full name - finish the template string
-//     h3.textContent = `${business.businessName}`;
-//     p.textContent =`${business.address}`;
-//     p2.textContent =`Membership Level: ${business.membership_lvl}`;
-//     p3.textContent =`Person of Contact: ${business.contactName}`
-    
-
-//     // Build the image portrait by setting all the relevant attribute
-//     logo.setAttribute('src', business.logo);
-//     logo.setAttribute('alt', `Logo for ${business.businessName}`);
-//     logo.setAttribute('loading', 'lazy');
-//     logo.setAttribute('width', '300');
-//     logo.setAttribute('height', 'auto');
-    
-
-//   //   p2.setAttribute('a', 'href');
-//   //   p2.setAttribute('src', directory.websiteurl);
-
-//     // Append the section(card) with the created elements
-//     card.appendChild(logo);
-//     card.appendChild(h3);
-//     card.appendChild(p2);
-//     card.appendChild(p3)
-//     card.appendChild(p);
-    
-
-//     cards.appendChild(card);
-//   }) // end of forEach loop
-// })
 
