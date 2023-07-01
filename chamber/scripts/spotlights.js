418e9b1f4ede484c2a3f7902cf0e51b3582d47f1
jessbarzee <bar19007@byui.edu>
2023-06-30 18:01:26 -0600

    json and contact page

--- chamber/scripts/spotlights.js
+++ chamber/scripts/spotlights.js
@@ -1,10 +1,21 @@
-const url = 'https://jessbarzee.github.io/wdd230/chamber/json/data.json';
+const urlSpotlights = 'https://jessbarzee.github.io/wdd230/chamber/json/data.json';
 
 async function getDirectoryData() {
-    const response = await fetch(url);
+    const response = await fetch(urlSpotlights);
     const data = await response.json();
+    console.log(data)
     // console.table(data.prophets);  // note that we reference the prophet array of the data object given the structure of the json file
-    displayData(data.directory);
+    let filterSpotlights = []
+    filterSpotlights = data.directory.filter(item => item.membership_lvl === "Gold" || item.membership_lvl === "Silver")
+    
+    let spotlights = []
+    for (let i = 0; i < 3; i++) {
+        let random = Math.floor(Math.random() * filterSpotlights.length)
+        console.log(filterSpotlights)
+        spotlights.push(filterSpotlights.splice(random, 1))
+    }
+    console.log(spotlights)
+    displayData(spotlights);
   }
   
   getDirectoryData();
@@ -17,22 +28,20 @@ const displayData = ((directory) => {
   directory.forEach((business) => {
     // Create elements to add to the div.cards element
     let card = document.createElement('section');
-    let h3 = document.createElement('h3');
+    let h2 = document.createElement('h2');
     let p = document.createElement('p');
-    let p2 = document.createElement('p');
     let a = document.createElement('a');
     let logo = document.createElement('img');
 
-    // Build the h2 content out to show the prophet's full name - finish the template string
-    h3.textContent = `${business.businessName}`;
-    p.textContent =`${business.address}`;
-    p2.textContent =`Membership Level: ${business.membership_lvl}`;
-    a.textContent =`Website: ${business.websiteurl}`;
+    // Build content 
+    h2.textContent = `${business[0].businessName}`;
+    p.textContent =`${business[0].description}`;
+    a.textContent =`Website: ${business[0].websiteurl}`;
     
 
     // Build the logo image by setting all the relevant attribute
-    logo.setAttribute('src', business.logo);
-    logo.setAttribute('alt', `Logo for ${business.businessName}`);
+    logo.setAttribute('src', business[0].logo);
+    logo.setAttribute('alt', `Logo for ${business[0].businessName}`);
     logo.setAttribute('loading', 'lazy');
     logo.setAttribute('width', '300');
     logo.setAttribute('height', 'auto');
@@ -42,10 +51,9 @@ const displayData = ((directory) => {
 
     // Append the section(card) with the created elements
     card.appendChild(logo);
-    card.appendChild(h3);
-    card.appendChild(p2);
-    card.appendChild(a)
+    card.appendChild(h2);
     card.appendChild(p);
+    card.appendChild(a)
     
 
     cards.appendChild(card);
