e2a70d59de53b25df74557ffff2dfeef77af31bd
jessbarzee <bar19007@byui.edu>
2023-07-15 19:05:55 -0600

    json

--- bountiful/scripts/fruitData.js
+++ bountiful/scripts/fruitData.js
@@ -1,51 +1,51 @@
-const url = 'https://brotherblazzard.github.io/canvas-content/fruit.json';
+const url = 'https://jessbarzee.github.io/wdd230/bountiful/json/fruit.json';
 
-async function getDirectoryData() {
+async function getFruitData() {
     const response = await fetch(url);
     const data = await response.json();
-    // console.table(data.prophets);  // note that we reference the prophet array of the data object given the structure of the json file
-    displayData(data.directory);
+    //console.table(data.fruits);  // note that we reference the prophet array of the data object given the structure of the json file
+    displayData(data.fruits);
   }
   
-  getDirectoryData();
+  getFruitData();
 
-const displayData = ((directory) => {
-    const cards = document.querySelector('div.directory_cards'); // select the output container element
+const displayData = ((fruits) => {
+     const elements = document.querySelector('div.elements'); // select the output container element
   
-    directory.forEach((business) => {
+    fruits.forEach((fruit) => {
       // Create elements to add to the div.cards element
-      let card = document.createElement('section');
-      let h3 = document.createElement('h3');
-      let p = document.createElement('p');
-      let p2 = document.createElement('p');
-      let a = document.createElement('a');
-      let logo = document.createElement('img');
+      let element = document.createElement('fieldset');
+      let fruit_name = document.createElement('input');
+      // let p = document.createElement('p');
+      // let p2 = document.createElement('p');
+      // let a = document.createElement('a');
+      // let logo = document.createElement('img');
   
       // Build the h2 content out to show the prophet's full name - finish the template string
-      h3.textContent = `${business.businessName}`;
-      p.textContent =`${business.address}`;
-      p2.textContent =`Membership Level: ${business.membership_lvl}`;
-      a.textContent=`Website: ${business.websiteurl}`;
+      fruit_name.textContent = `${fruit.name}`;
+      // p.textContent =`${business.address}`;
+      // p2.textContent =`Membership Level: ${business.membership_lvl}`;
+      // a.textContent=`Website: ${business.websiteurl}`;
       
   
       // Build the image portrait by setting all the relevant attribute
-      logo.setAttribute('src', business.logo);
-      logo.setAttribute('alt', `Logo for ${business.businessName}`);
-      logo.setAttribute('loading', 'lazy');
-      logo.setAttribute('width', '300');
-      logo.setAttribute('height', 'auto');
+      // logo.setAttribute('src', business.logo);
+      // logo.setAttribute('alt', `Logo for ${business.businessName}`);
+      // logo.setAttribute('loading', 'lazy');
+      // logo.setAttribute('width', '300');
+      // logo.setAttribute('height', 'auto');
       
 
-      a.setAttribute('href', directory.websiteurl);
+      // a.setAttribute('href', directory.websiteurl);
   
       // Append the section(card) with the created elements
-      card.appendChild(logo);
-      card.appendChild(h3);
-      card.appendChild(p2);
-      card.appendChild(p);
-      card.appendChild(a)
+      element.appendChild(fruit_name);
+      // element.appendChild(h3);
+      // element.appendChild(p2);
+      // element.appendChild(p);
+      // element.appendChild(a)
       
   
-      cards.appendChild(card);
+      elements.appendChild(element);
     }) // end of forEach loop
 })
